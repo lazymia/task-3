c9356927cf843965feac4117b2d57d1038d62ea6
Chiamaka <hello.chiamaka@gmail.com>
2022-11-11 07:38:11 +0100

    removed the equal sign

--- test.sql
+++ test.sql
@@ -1,8 +1,8 @@
 -- Given the Contacts table below, write a SQL statement to get all contacts that have both email and phone values populated
 SELECT *
 FROM Contacts
-WHERE email LIKE = '%.com'
-AND phone LIKE = '60%'
+WHERE email LIKE '%.com'
+AND phone LIKE '60%'
 
 
 --  Given the Users table below, write a SQL statement to get the count of users per groupId
